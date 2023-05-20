38cf5ed69b1678ae11700d702abd4d7a37b1df83
karthik-sanju <karthiksanju2001@gmail.com>
2023-05-20 17:36:28 +0530

    added

---
+++ app.java
@@ -0,0 +1,20 @@
+
+public class app
+{
+    public static int add (int first , int second )
+    {
+      int result = first + second;
+      return result;
+
+    }
+    public static void main( String[] args )
+    {
+      int addResult  = Arithmatic.add(10,90);
+
+          System.out.println( "add result  :" +addResult);
+      }
+
+
+
+}
+
